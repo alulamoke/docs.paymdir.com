ef6336af5d033e64258218f4c5d07ceba8b5e4bb
alulamoke <alulam1926@gmail.com>
2024-12-04 18:54:12 +0300

    global background added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import { baseUrl, createMetadata } from '@/lib/metadata';
 
 import './global.css';
+import { Background } from '@/components/Background';
 
 export const metadata = createMetadata({
   title: {
@@ -23,7 +24,10 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={poppins.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <Background />
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
