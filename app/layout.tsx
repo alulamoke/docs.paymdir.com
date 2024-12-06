0ef776e17c5294d7b0e052e4b642b0eea1e0cf71
alulamoke <alulam1926@gmail.com>
2024-12-06 23:40:08 +0300

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import type { ReactNode } from 'react';
 import { Poppins } from 'next/font/google';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { baseUrl, createMetadata } from '@/lib/metadata';
 
 import './global.css';
 import { Background } from '@/components/Background';
+
+export const metadata = createMetadata({
+  title: {
+    template: 'PayMdir | %s',
+    default: 'PayMdir',
+  },
+  description:
+    "Comprehensive API documentation, integration guides, and resources to help you seamlessly connect to PayMdir's powerful payment system. Build, test, and deploy with ease.",
+  metadataBase: baseUrl,
+});
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
